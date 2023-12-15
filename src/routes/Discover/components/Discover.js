8244034b877b60ccd2e6633db1d0adc96901a159
dwidharmas <dwi.dharma97@gmail.com>
2023-12-15 21:05:11 +0800

    Fetch All Data

--- src/routes/Discover/components/Discover.js
+++ src/routes/Discover/components/Discover.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
+import config from '../../../config';
 
 export default class Discover extends Component {
   constructor() {
@@ -11,7 +12,96 @@ export default class Discover extends Component {
       playlists: [],
       categories: []
     };
+
   }
+  
+  componentDidMount() {
+    this.fetchToken();
+  }
+
+  fetchToken = async () => {
+    try {
+      const response = await fetch(`${config.api.authUrl}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `grant_type=client_credentials&client_id=${config.api.clientId}&client_secret=${config.api.clientSecret}`,
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch token');
+      }
+
+      const data = await response.json();
+      const token = data.access_token;
+
+      // Call the function to fetch data using the obtained token
+      this.fetchNewRelease(token);
+      this.fetchPlaylist(token);
+      this.fetchCategories(token)
+    } catch (error) {
+      console.error('Error fetching token:', error.message);
+    }
+  };
+
+  fetchNewRelease = async (token) => {
+    try {
+      const response = await fetch(`${config.api.baseUrl}/browse/new-releases`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch new releases');
+      }
+
+      const data = await response.json();
+      this.setState({ newReleases: data.albums.items });
+    } catch (error) {
+      console.error('Error fetching new releases:', error.message);
+    }
+  };
+
+  fetchPlaylist = async (token) => {
+    try {
+      const response = await fetch(`${config.api.baseUrl}/browse/featured-playlists`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch new releases');
+      }
+
+      const data = await response.json();
+      this.setState({ playlists: data.playlists.items });
+    } catch (error) {
+      console.error('Error fetching new releases:', error.message);
+    }
+  };
+
+  fetchCategories = async (token) => {
+    try {
+      const response = await fetch(`${config.api.baseUrl}/browse/categories`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch new releases');
+      }
+
+      const data = await response.json();
+      this.setState({ categories: data.categories.items });
+    } catch (error) {
+      console.error('Error fetching new releases:', error.message);
+    }
+  };
+
 
   render() {
     const { newReleases, playlists, categories } = this.state;
